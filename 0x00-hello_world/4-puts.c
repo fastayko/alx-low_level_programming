4481cb327a24883f24b5309d0231101e669dc42f
Fastayko <20912455+fastayko@users.noreply.github.com>
2022-12-08 23:16:37 +0100

    e zebbi !

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char slm[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(slm);
+
+
+	return (0);
+}
