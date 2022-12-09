dc242a3e2c1f4a0d3127bbaf620686d56c422e82
Fastayko <20912455+fastayko@users.noreply.github.com>
2022-12-10 00:47:17 +0100

    y

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch == 'q' || ch == 'e')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Use `putchar` function to print the alphabet in lowercase.
+ * Return: 0
+ */
+int main(void)
+{
+	int c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+		if (c <= '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
